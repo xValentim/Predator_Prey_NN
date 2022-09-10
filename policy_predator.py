783f5375376628bc6c8d0b35726d9a6a89e13b13
Gabriel Valentim <gabriel.valentim.blis@hotmail.com>
2022-09-09 22:49:22 -0300

    Finish Environment and Trainning model

--- policy_predator.py
+++ policy_predator.py
@@ -4,14 +4,20 @@ from tensorflow.keras.optimizers import Adam
 import numpy as np
 
 class Policy:
-    def __init__(self):
-        self.alpha = 0.01
-        self.policy = keras.models.Sequential([
-            keras.layers.Dense(25, activation="relu", input_shape=[25]),
-            keras.layers.Dense(10, activation="relu"),
-            keras.layers.Dense(5, activation="softmax"),
-        ])
-        self.policy.compile(optimizer=Adam(learning_rate=self.alpha))
+    def __init__(self, load=False, pos=(0, 0)):
+        self.pos = pos
+        if not load:
+            self.alpha = 0.01
+            self.policy = keras.models.Sequential([
+                keras.layers.Dense(25, activation="relu", input_shape=[25]),
+                keras.layers.Dense(10, activation="relu"),
+                keras.layers.Dense(5, activation="softmax"),
+            ])
+            self.policy.compile(optimizer=Adam(learning_rate=self.alpha))
+        else:
+            path_best_model = 'Training/my_model_PP_2'
+            self.policy = keras.models.load_model(path_best_model)
+        
         
     def __call__(self, state):
         pi = self.policy(state)
@@ -25,16 +31,20 @@ def get_obs(array, pos):
         for jj in range(-2, 3, 1):
             row.append(array[(i + ii) % len(array)][(j + jj) % len(array)])
         obs.append(row)     
-    return obs
+    return np.array(obs)
 
-def apply_action(array, pos, action):
+def apply_action(array, pos, action, model=None):
     i, j = pos
     if action == 0:
         array[i][j], array[i][(j + 1) % len(array)] = array[i][(j + 1) % len(array)], array[i][j]
+        model.pos = (i, (j + 1) % len(array))
     elif action == 1:
-        array[i][j], array[i][(j - 1)] = array[i][(j - 1)], array[i][j]
+        array[i][j], array[i][(j - 1) % len(array)] = array[i][(j - 1) % len(array)], array[i][j]
+        model.pos = (i, (j - 1) % len(array))
     elif action == 2:
-        array[i][j], array[i - 1][j] = array[i - 1][j], array[i][j]
+        array[i][j], array[(i - 1)  % len(array)][j] = array[(i - 1) % len(array)][j], array[i][j]
+        model.pos = ((i - 1) % len(array), j)
     elif action == 3:
-        array[i][j], array[i + 1][j] = array[(i + 1) % len(array)][j], array[i][j]
+        array[i][j], array[(i + 1) % len(array)][j] = array[(i + 1) % len(array)][j], array[i][j]
+        model.pos = ((i + 1) % len(array), j)
     
--- predator_prey.py
+++ predator_prey.py
@@ -1,3 +1,4 @@
+from multiprocessing import allow_connection_pickling
 import random
 import numpy as np
 import pygame
@@ -13,16 +14,16 @@ white = (255, 255, 255)
 red = (255, 0, 0)
 background = white
 ball_color = gray
-fps = 60
+fps = 50
 
 '''
 0 - empty
 1 - predator
 2 - prey
 '''
-types = [0, 0, 2]
+types = [0, 0, 0, 0, 2]
 color = [gray, red, white]
-model = Policy()
+model = Policy(load=True, pos=(0, 0))
 
 if len(sys.argv) != 5:
     print("Error Usage. You must type this form: python predator_prey.py a b c")
@@ -31,8 +32,11 @@ else:
     t = 0
     particules = np.array([[random.choice(types) for i in range(0, largura, 20)] for j in range(0, altura, 20)])
     print(len(particules))
-    for k in range(20):
-        i, j = random.randint(0, len(particules) - 1), random.randint(0, len(particules) - 1)
+    L = len(particules)
+    for k in range(1):
+        # i, j = random.randint(0, len(particules) - 1), random.randint(0, len(particules) - 1)
+        i, j = L // 2, L // 2
+        model.pos = (i, j)
         particules[i][j] = 1
     pygame.init()
     relogio = pygame.time.Clock()
@@ -52,7 +56,8 @@ else:
 
         
         window.fill(background)
-        i, j = random.randint(0, len(particules) - 1), random.randint(0, len(particules) - 1)
+        # i, j = random.randint(0, len(particules) - 1), random.randint(0, len(particules) - 1)
+        i, j = model.pos
         particule = particules[i][j]
         adress = [(i, (j + 1) % len(particules)), (i, (j - 1) % len(particules)), ((i - 1) % len(particules), j), ((i + 1) % len(particules), j)]
         right, left, up, down = particules[adress[0][0]][adress[0][1]], particules[adress[1][0]][adress[1][1]], particules[adress[2][0]][adress[2][1]], particules[adress[3][0]][adress[3][1]]
@@ -72,9 +77,18 @@ else:
             else:
                 z = random.random()
                 if z < float(p):
+                    '''
+                    0 - baixo
+                    1 - cima
+                    2 - esquerda
+                    3 - direita
+                    '''
                     obs = np.array(get_obs(particules, (i, j))).reshape(1, -1)
-                    action = model(obs).numpy().argmax()
-                    apply_action(particules, (i, j), action)
+                    # action = model(obs).numpy().argmax()
+                    action = model(obs.reshape(1, -1)).numpy().argmax()
+                    # action = random.randint(0, 3)
+                    print(action)
+                    apply_action(particules, (i, j), action, model=model)
 
         elif particule == 2:
             for k in range(4):
@@ -90,13 +104,6 @@ else:
             for j in range(1, len(particules[i]) - 1):
                 pygame.draw.rect(window, color[particules[i][j]], (i * 20 + 1, j * 20 + 1, 20, 20))
 
-
-                
-                    
-                    
-
-
-                
         t += 1
         relogio.tick(fps)
         pygame.display.update()
